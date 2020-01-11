b6199dabfdbd1b88a4d4b00d9e6460b5a9eba353
franklin <uhexos@gmail.com>
2020-01-11 12:36:21 +0000

    added ability to delete category

--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -3,6 +3,7 @@ import { Card, CardBody, Button, Container, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import CardTitle from 'reactstrap/lib/CardTitle'
 import Col from 'reactstrap/lib/Col';
+import Modals from './NotificationModal';
 
 export class CategoryList extends Component {
     state = { categories: [] }
@@ -23,6 +24,22 @@ export class CategoryList extends Component {
                 this.setState({ categories: data });
             });
     }
+    deleteCategory = (category_id) => {
+        // delete return no json response
+        fetch(`http://localhost:8000/api/categories/${category_id}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `JWT ${localStorage.getItem('token')}`
+            }
+        })
+            .then(() => {
+                // remove the just deleted category from the state and update state without making another api call.
+                this.setState({categories: this.state.categories.filter((category) =>{ 
+                    return category['id'] !==  category_id
+                })});
+            })
+
+    }
     render() {
         let { categories } = this.state
         return (
@@ -30,16 +47,26 @@ export class CategoryList extends Component {
                 <Container>
                     <Row>
                         {categories.length > 0 ? categories.map(category => (
-                            <Col md="3" key={category.id}>
-                                <Card className="shadow" >
+                            <Col lg="3" md="6" key={category.id}>
+                                <Card className="shadow my-2" >
                                     <CardBody>
                                         <CardTitle>
                                             <h3>{category.title}</h3>
                                             <p>{category.description}</p>
                                         </CardTitle>
-                                        <Link to={`/admin/categories/${category.id}`}>
-                                            <Button type="button" color="success">Edit Category</Button>
-                                        </Link>
+                                        <Row>
+
+                                            <Link to={`/admin/categories/${category.id}`}>
+                                                <Button size="md" type="button" color="success" className="mx-2">Edit</Button>
+                                            </Link>
+                                            <Modals
+                                                title="Confirm Deletion !"
+                                                message={`You are about to delete the category "${category.title}" permanently are you sure you want to do this? This will delete all courses belonging to it. Did you want to rename ?`}
+                                                buttonText="Delete"
+                                                buttonColor="danger"
+                                                action={() => this.deleteCategory(category.id)}
+                                            />
+                                        </Row>
                                     </CardBody>
                                 </Card>
                             </Col>
