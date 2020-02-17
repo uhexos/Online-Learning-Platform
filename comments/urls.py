5b4f6231d116d6e23287181ab7ac58dc6d9e4b61
franklin <uhexos@gmail.com>
2020-02-17 01:32:28 +0000

    removed comment app

--- comments/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    path('cart/new/', CartList.as_view(), name='cart-list')
-]
--- course_site/settings.py
+++ course_site/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'courses',
     'cart',
-    'comments',
 ]
 
 MIDDLEWARE = [
